4afb4d0d8f61f8ea4948d7f5225a0b859ec25031
KimAman <chalgaro46@gmail.com>
2023-06-03 18:44:45 +0900

    index phps

--- src/js/check_login.js
+++ src/js/check_login.js
@@ -1,33 +1,19 @@
-var object
-function checkLoginStatus(object) {
+function checkLoginStatus(object, button) {
     fetch('php/get_user_id.php')
         .then(response => response.text())
         .then(userID => {
             var isLoggedIn = userID != "";
-            handleLoginStatus(isLoggedIn, object); // 로그인 상태 체크 및 버튼 표시 처리 함수 호출
+            handleLoginStatus(isLoggedIn, object, button); // 로그인 상태 체크 및 버튼 표시 처리 함수 호출
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
 
-// cart modal
-// Get the button element to open the modal
-const openModalButton = document.querySelectorAll('.openCartModal');
 const closeModalButton = document.querySelector('.closeCartModal');
-// Get the modal element
 const modalOverlay = document.querySelector('#modal-overlay');
 
-// Attach event listener to the button
-
-openModalButton.forEach(button => {
-button.addEventListener('click', () => {
-    checkLoginStatus('cart');
-    }); 
-});
-
-
-function handleLoginStatus(isLoggedIn,object) {
+function handleLoginStatus(isLoggedIn,object,button) {
     switch(object){
         case 'loginButton':
             var loginButton = document.getElementById('loginButton');
@@ -45,38 +31,20 @@ function handleLoginStatus(isLoggedIn,object) {
             }
             break;
         case 'cart':
-            if (isLoggedIn) {0
+            if (isLoggedIn) {
             // 로그인 상태인 경우
                 //cart modal 등장
-                openModalButton.forEach(button => {
+                
                 button.addEventListener('click', () => {
                     modalOverlay.classList.remove('d-none');
-                    }); 
                 });
-
                 closeModalButton.addEventListener('click',() => {
                     modalOverlay.classList.add('d-none');
                 });
 
                 // 아이템 받아오기
 
-                // 갯수 조절 버튼
-                var inputNumber = document.getElementById('productsNumbers');
-
-                const incrementButton = document.querySelector(".increment-btn");
-                const decrementButton = document.querySelector(".decrement-btn");
                 
-                incrementButton.addEventListener("click", () => {
-                    var currentValue = parseInt(inputNumber.value);
-                    inputNumber.value = currentValue + 1;
-                });
-                
-                decrementButton.addEventListener("click", () => {
-                    var currentValue = parseInt(inputNumber.value);
-                    if (currentValue > 0) {
-                    inputNumber.value = currentValue - 1;
-                    }
-                });
 
                 //해당 아이템을 장바구니 테이블에 넣기
 
@@ -84,8 +52,11 @@ function handleLoginStatus(isLoggedIn,object) {
 
             } else {
             // 로그아웃 상태인 경우
+            button.addEventListener('click', () => {
                 alert("로그인이 필요합니다.");
                 window.location.href = "login.html";
+            });
+               
             }
             break;
         case 'favorite':
@@ -95,18 +66,16 @@ function handleLoginStatus(isLoggedIn,object) {
                 // 불러온 정보 확인 후 favorite 표시
 
                 // 버튼 토글 기능
-                    favoriteButtons.forEach(button => {
-                        button.addEventListener('click', () => {
-                        button.classList.toggle('btn-outline-danger');
-                        });
-                    });
+                    
     
                 // 해당 상품 favorite 테이블에 추가
     
                 } else {
                 // 로그아웃 상태인 경우
-                    alert("로그인이 필요합니다.");
-                    window.location.href = "login.html";
+                    button.addEventListener('click', () => {
+                        alert("로그인이 필요합니다.");
+                        window.location.href = "login.html";
+                    });
                 }
                 break;
     }
--- src/js/sidebars.js
+++ src/js/sidebars.js
@@ -1,8 +1,15 @@
-/* global bootstrap: false */
-(() => {
-  'use strict'
-  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(tooltipTriggerEl => {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})()
+// var btnToggle = document.querySelectorAll('.btn-toggle');
+
+// btnToggle.forEach(function (button) {
+//   button.addEventListener('mouseenter', function () {
+//     var target = button.getAttribute('data-bs-target');
+//     var collapse = document.querySelector(target);
+//     collapse.classList.add('show');
+//   });
+
+//   button.addEventListener('mouseleave', function () {
+//     var target = button.getAttribute('data-bs-target');
+//     var collapse = document.querySelector(target);
+//     collapse.classList.remove('show');
+//   });
+// });
